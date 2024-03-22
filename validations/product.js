9f6af749d32ebebfa2a73bede009a8ee8f5f085e
Shetriga <mahmoud.atef98@outlook.com>
2024-03-22 15:55:32 +0200

    Fixed some bugs

--- validations/product.js
+++ validations/product.js
@@ -10,6 +10,4 @@ exports.putProductValidations = [
   check("name", "Name is missing").notEmpty(),
   check("quantity", "Quantity is missing").notEmpty(),
   check("unitPrice", "Unit Price is missing").notEmpty(),
-  check("offerPrice", "Offer Price is missing").notEmpty(),
-  check("expiryDate", "Expiry Date is missing").notEmpty(),
 ];
