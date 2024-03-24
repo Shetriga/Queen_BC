94ec91dbfa1746c756352a13a15b743a6c272678
Shetriga <mahmoud.atef98@outlook.com>
2024-03-25 01:09:21 +0200

    Fixed some bugs

--- app.js
+++ app.js
@@ -12,10 +12,7 @@ const bodyParser = require("body-parser");
 // CORS Policy headers and permissions
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+  res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, DELETE, PUT"
