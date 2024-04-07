1eabbb72e6f10aad106e211209d5e10e7fc3ce40
Shetriga <mahmoud.atef98@outlook.com>
2024-04-08 01:04:16 +0200

    Fixed some bugs

--- validations/asset.js
+++ validations/asset.js
@@ -4,7 +4,6 @@ exports.postAddAssetValidations = [
   check("assetName", "Asset Name is missing").notEmpty(),
   check("quantity", "Quantity is missing").notEmpty(),
   check("purchaseDate", "Purchase Date is missing").notEmpty(),
-  check("lastUpdateDate", "Last Update Date is missing").notEmpty(),
   check("unitPrice", "Unit Price Date is missing").notEmpty(),
   check("totalPrice", "Total Price Date is missing").notEmpty(),
 ];
